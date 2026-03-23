84d31ea8e7a3aa450f2851df5787f516c7497dd0
1a2n3k4u5r <ankur1236876@gmail.com>
2026-03-23 20:35:03 +0530

    fix refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -193,7 +193,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 const refreshAccessToken = asyncHandler(async (req,res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-  if (incomingRefreshToken){
+  if (!incomingRefreshToken){
     throw new ApiError(401, "unauthorized request")
   }
 
@@ -209,7 +209,7 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
       throw new ApiError(401, "unauthorized request")
      }
   
-     if(incomingRefreshToken !== urser?.refreshToken) {
+     if(incomingRefreshToken !== user?.refreshToken) {
       throw new ApiError(401, "Invalid refresh token is expired or used")
      }
   
