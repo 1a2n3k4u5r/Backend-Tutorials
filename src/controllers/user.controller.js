8012eeea1c56972feb822cec86bd4b40cec51f45
1a2n3k4u5r <ankur1236876@gmail.com>
2026-03-25 11:08:39 +0530

    Add getWatchHistory function to retrieve user's watched videos with owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -424,6 +424,62 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
   new ApiResponse(200, channel[0]," user channel fetched successfully")
  ) 
 
+ const getWatchHistory = asyncHandler(async(req,res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId (req.user._id)
+      }
+  },
+  {
+         $lookup: {
+          from: "videos",
+          localField: "watchHistory",
+          foreignField: "_id",
+          as: "watchHistory",
+          pipeline: [
+            {
+              
+         $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner",
+          pipeline: [
+            {
+              $project: {
+                fullName: 1,
+                username: 1,
+                avatar: 1
+              }
+            }
+          ]
+         }
+        
+            },
+            {
+              $addField:{
+                owner: {
+                  $first: "$owner"
+                }
+              }
+            }
+          ]
+         }
+       }, 
+  
+])
+
+return res
+.status(200)
+.json(
+  new ApiResponse(
+    200,
+    user[0].getWatchHistory,
+    "Watch history fetched succesfully"
+  )
+)
+ })
 export {
     registerUser,
     loginUser,
@@ -434,7 +490,10 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory 
+    
+
 
 }
 
