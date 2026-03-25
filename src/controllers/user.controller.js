8e9e632ab7073110dcd2ebd1327d20a90d80062d
1a2n3k4u5r <ankur1236876@gmail.com>
2026-03-25 10:00:10 +0530

    Add getUserChannelProfile function to fetch channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -350,6 +350,79 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
 
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res) => {
+        const {username} = req.params
+
+        if(!username?.trim()) {
+          throw new ApiError(400,"username is missing")
+        }
+
+       const channel = await User.aggregate([
+        {
+           $match: {
+            username: username?.toLowerCase()
+           }
+       },
+
+       {
+         $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers"
+         }
+       }, 
+
+       {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo"
+        }
+       },
+
+       {
+        $addFiels:{
+          subscribersCount: {
+            $size: "$subscribers"
+          },
+        channelsSubscribeToCount:{
+           $size:"$subscribedTo"
+        },
+        isSubscribed:{
+          $cond: {
+            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+       }
+      },
+      {
+        $project: {
+          fullName: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelsSubscribeToCount:1,
+          isSubscribed:1,
+          avatar:1,
+          coverImage: 1,
+          email: 1
+        }
+      }
+        ])      // aggregate ek method hai jo ki array lata hai  or array ka andar pipeline liki jati hai or $ sign means it is a field
+})
+
+ if(!channel?.length) {
+  throw new ApiError(404, "channel does not exists")
+ }
+
+ return res
+ .status(200)
+ .json(
+  new ApiResponse(200, channel[0]," user channel fetched successfully")
+ ) 
 
 export {
     registerUser,
@@ -360,7 +433,9 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
+
 }
 
 //kuki ya ek array hai or array pa bahut sara method hota hai to hum .map lga sakta  hai lakin yha pa hum .some ka upar hum condition lga ka check karta hai or ya true or false return karta hai.
